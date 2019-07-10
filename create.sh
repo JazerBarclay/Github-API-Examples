29ee87265f25ab7211da9dc4ee17221eafb13276
jazer <jazer@tora.tech>
2019-07-10 12:26:18 +0100

    Created script to make and pull repo

---
+++ create.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+display_usage() {
+	echo "Please ensure your username and key is set"
+	echo "Usage: $0 project name goes here"
+}
+
+username=joebloggs
+token=0123456789
+projectslocation=~/projects
+
+if [ $# -le 1 ]
+then
+	display_usage
+	exit 1
+fi
+
+projectname=$(echo $@ | tr " " -)
+#projectname=$(echo $@ | sed -e "s/\b\(.\)/\u\1/g" | tr " " -)
+#projectname=$(echo $1 | sed -e "s/\b\(.\)/\u\1/g" | tr " " -)
+#projectname=$1
+echo "Creating project: $projectname"
+
+curl -X POST \
+  https://api.github.com/user/repos \
+  -u $username:$token \
+  -H 'Content-Type: application/json' \
+  -d '{ "name": "'$projectname'", "private": false, "auto_init": true }'
+  
+pushd $projectslocation && git clone git@github.com:$username/$projectname.git && popd
+
+
